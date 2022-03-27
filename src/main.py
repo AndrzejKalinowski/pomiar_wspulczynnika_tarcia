0cd96370f3e1925c5a861a74fe3e2f2bd0bdcca9
Andrzej Kalinowski <jekrekk75@gmail.com>
2022-03-27 21:56:01 +0200

    delta distance

--- src/main.py
+++ src/main.py
@@ -7,25 +7,33 @@ if __name__ == "__main__":
     x = 0
     y = 0
     z = 0
+    distance = 0
     print("hello")
     time.sleep(2)
     print("starting mesurment")
     while True:
+        prev_distance = distance
+        # reading data from the arduino
         line = ser.readline()
-        # print(line)
+        # interpreting data form arduion
         if line[0] == 88:
-            # print(line)
             x = float(line[2:6])
-        elif line[0] == 89:
-            y = float(line[2:6])
         elif line[0] == 90:
             z = float(line[2:6])
-        elif line[0] == 44:
-            distance = float(line[2:6])
+        elif line[0] == 68:
+            try:
+                distance = float(line[2:6])
+            except ValueError:
+                distance = -1
         
-        # print(x, y, z)
+        #calculating the angle
         try:
             alpha = 90 - math.degrees(np.arctan(z/x))
         except ZeroDivisionError:
             alpha = 0
-        print(alpha)
+        
+        delta_distance = distance - prev_distance
+        if delta_distance >= 5 and delta_distance < 50:
+            pass
+        # print(alpha)
+        print(delta_distance)
