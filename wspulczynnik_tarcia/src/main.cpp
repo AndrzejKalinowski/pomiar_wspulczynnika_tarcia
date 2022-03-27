a03566d3666fb49dd51a6799c1a873cda3f7bf88
Andrzej Kalinowski <jekrekk75@gmail.com>
2022-03-27 20:16:54 +0200

    init

---
+++ wspulczynnik_tarcia/src/main.cpp
@@ -0,0 +1,61 @@
+#include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include "DFRobot_VL53L0X.h"
+
+DFRobot_VL53L0X sensor;
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  //setupt motion detection
+  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+  mpu.setMotionDetectionThreshold(1);
+  mpu.setMotionDetectionDuration(20);
+  mpu.setInterruptPinLatch(true);	// Keep it latched.  Will turn off when reinitialized.
+  mpu.setInterruptPinPolarity(true);
+  mpu.setMotionInterrupt(true);
+  //Set I2C sub-device address
+  sensor.begin(0x50);
+  //Set to Back-to-back mode and high precision mode
+  sensor.setMode(sensor.eContinuous,sensor.eHigh);
+  //Laser rangefinder begins to work
+  sensor.start();
+  Serial.println("");
+  delay(100);
+}
+
+void loop() {
+
+   /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print("X ");
+  Serial.println(a.acceleration.x);
+  Serial.print("Y ");
+  Serial.println(a.acceleration.y);
+  Serial.print("Z ");
+  Serial.println(a.acceleration.z);
+
+  Serial.print("D ");
+  Serial.println(sensor.getDistance());
+
+  delay(100);
+}
