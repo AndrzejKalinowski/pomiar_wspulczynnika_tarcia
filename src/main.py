c634e34d5010bc3896066ccb7e6b363b6bcfc44b
Andrzej Kalinowski <jekrekk75@gmail.com>
2022-03-27 22:26:54 +0200

    displaying values using pygame

--- src/main.py
+++ src/main.py
@@ -2,6 +2,18 @@ import numpy as np
 import serial
 import time
 import math
+import pygame
+
+# pygame stuff
+pygame.init()
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+X = 600
+Y = 600
+display_surface = pygame.display.set_mode((X, Y))
+pygame.display.set_caption('Show Text')
+font = pygame.font.Font('freesansbold.ttf', 32)
 
 def main():
     ser = serial.Serial('COM7', 115200)
@@ -36,12 +48,29 @@ def main():
             alpha = 0
         
         delta_distance = distance - prev_distance
-        if delta_distance >= 5 and delta_distance < 50:
+        if delta_distance >= 10 and delta_distance < 50:
             break
         # print(alpha)
         print(delta_distance)
+
+        display_surface.fill(white)
+        text = font.render(str(alpha), True, blue, white)
+        textRect = text.get_rect()
+        textRect.center = (X // 2, Y // 2) 
+        display_surface.blit(text, textRect)
+        pygame.display.update()
+
     print("movment detected")
     print("alpha: ", alpha)
-    print("f = ", math.tan(math.radians(alpha)))
+    print("delta distance:", delta_distance)
+    f = math.tan(math.radians(alpha))
+    print("f = ", f)
+    display_surface.fill(white)
+    text = font.render("f = " + str(f), True, green, white)
+    textRect = text.get_rect()
+    textRect.center = (X // 2, Y // 2) 
+    display_surface.blit(text, textRect)
+    pygame.display.update()
+    time.sleep(100)
 if __name__ == "__main__":
     main()
