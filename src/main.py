4ccc220985305be8b4c9e02aa55eb6461f7952e3
Andrzej Kalinowski <jekrekk75@gmail.com>
2022-03-27 21:38:40 +0200

    claculating angle

--- src/main.py
+++ src/main.py
@@ -1,12 +1,31 @@
 import numpy as np
 import serial
 import time
+import math
 ser = serial.Serial('COM7', 115200)
 if __name__ == "__main__":
+    x = 0
+    y = 0
+    z = 0
     print("hello")
     time.sleep(2)
     print("starting mesurment")
     while True:
         line = ser.readline()
-        if line[0] == 68:
-            print(line)
+        # print(line)
+        if line[0] == 88:
+            # print(line)
+            x = float(line[2:6])
+        elif line[0] == 89:
+            y = float(line[2:6])
+        elif line[0] == 90:
+            z = float(line[2:6])
+        elif line[0] == 44:
+            distance = float(line[2:6])
+        
+        # print(x, y, z)
+        try:
+            alpha = 90 - math.degrees(np.arctan(z/x))
+        except ZeroDivisionError:
+            alpha = 0
+        print(alpha)
